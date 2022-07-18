ff5ab4a4b9074fc54e58d9975d13dac772de04ba
moonstal1506 <moonstal1506@gmail.com>
2022-07-18 14:22:35 +0900

    엔티티 생성

---
+++ order/src/main/java/com/example/order/Order.java
@@ -0,0 +1,34 @@
+package com.example.order;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Order {
+    private final UUID orderId;
+    private final UUID customerId;
+    private final List<OrderItem> orderItems;
+    private long discountAmount;
+    private OrderStatus orderStatus = OrderStatus.ACCEPTED;
+
+    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems, long discountAmount) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderItems = orderItems;
+        this.discountAmount = discountAmount;
+    }
+
+    public long totalAmount(){
+        //자바14 v.productPrice() * v.quantity()
+        Long beforeDiscount = orderItems.stream().map(v -> v.getProductPrice() * v.getQuantity())
+                .reduce(0L, Long::sum);
+        return beforeDiscount - discountAmount;
+    }
+
+    public void setDiscountAmount(long discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
---
+++ order/src/main/java/com/example/order/OrderItem.java
@@ -0,0 +1,31 @@
+package com.example.order;
+
+import java.util.UUID;
+
+
+//자바14 레코드 불변객체
+//public record OrderItem(UUID productId,long productPrice,long quantity){}
+
+public class OrderItem {
+    private final UUID productId;
+    private final long productPrice;
+    private final long quantity;
+
+    public OrderItem(UUID productId, long productPrice, int quantity) {
+        this.productId = productId;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public long getProductPrice() {
+        return productPrice;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+}
