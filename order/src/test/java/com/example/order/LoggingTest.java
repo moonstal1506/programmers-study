7a19fd6c076edafddca4659fb8df1f7a6e2eedcb
moonstal1506 <moonstal1506@gmail.com>
2022-07-22 09:59:06 +0900

    로깅

---
+++ order/src/test/java/com/example/order/LoggingTest.java
@@ -0,0 +1,26 @@
+package com.example.order;
+
+import com.example.order.order.OrderProperties;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class LoggingTest {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingTest.class);
+
+    @Test
+    void logging() throws Exception {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfiguration.class);
+
+        OrderProperties orderProperties = applicationContext.getBean(OrderProperties.class);
+
+        log.info("logger name=> {}", log.getName());
+        log.info("Version->{}", orderProperties.getVersion());
+        log.info("MinimumOrderAmount->{}", orderProperties.getMinimumOrderAmount());
+        log.info("supportVendors->{}", orderProperties.getSupportVendors());
+        log.info("description->{}", orderProperties.getDescription());
+    }
+}
