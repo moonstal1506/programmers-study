9332f36d2efeaa65035030080ca90b121ed07a54
moonstal1506 <moonstal1506@gmail.com>
2022-08-16 09:26:02 +0900

    SSL 인증서 적용

--- ssmc/src/main/java/com/prgrms/devcource/configures/WebSecurityConfigure.java
+++ ssmc/src/main/java/com/prgrms/devcource/configures/WebSecurityConfigure.java
@@ -28,23 +28,27 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-            .antMatchers("/me").hasAnyRole("USER", "ADMIN") //인증 영역
-            .anyRequest().permitAll() //익명영역
-            .and()
-            .formLogin() //스프링 시큐리티가 로그인 폼 자동 생성
-            .defaultSuccessUrl("/") //로그인 성공시 갈 곳
-            .permitAll() //로그인은 권한 필요 없음
-            .and()
-            .logout()
-            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))//얘도 디폴트임
-            .logoutSuccessUrl("/")
-            .invalidateHttpSession(true)//생략해도 무관 이미 기본값에 설정
-            .clearAuthentication(true)
-            .and()
-            .rememberMe()
-            .rememberMeParameter("remember-me")
-            .tokenValiditySeconds(300)
+                .authorizeRequests()
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN") //인증 영역
+                .anyRequest().permitAll() //익명영역
+                .and()
+                .formLogin() //스프링 시큐리티가 로그인 폼 자동 생성
+                .defaultSuccessUrl("/") //로그인 성공시 갈 곳
+                .permitAll() //로그인은 권한 필요 없음
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))//얘도 디폴트임
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)//생략해도 무관 이미 기본값에 설정
+                .clearAuthentication(true)
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(300)
+                .and()
+                .requiresChannel()
+//                .antMatchers("/api/**")
+                .anyRequest().requiresSecure()  //모든 요청은 시큐어(https) 필요
         ;
     }
 }
