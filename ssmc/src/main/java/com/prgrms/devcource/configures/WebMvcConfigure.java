8fdc367a9a32849f1ae2aab6e2a1e0c6ac4b544c
moonstal1506 <moonstal1506@gmail.com>
2022-08-17 16:57:42 +0900

    인증 이벤트

--- ssmc/src/main/java/com/prgrms/devcource/configures/WebMvcConfigure.java
+++ ssmc/src/main/java/com/prgrms/devcource/configures/WebMvcConfigure.java
@@ -1,9 +1,11 @@
 package com.prgrms.devcource.configures;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableAsync //Async어노테이션 활성화
 @Configuration
 public class WebMvcConfigure implements WebMvcConfigurer {
     @Override
