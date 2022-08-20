52e77c8451058c788edb111a2e9f7df49e575754
moonstal1506 <moonstal1506@gmail.com>
2022-08-20 15:08:53 +0900

    JWT 기반 API

--- ssmc/src/main/java/com/prgrms/devcource/configures/WebSecurityConfigure.java
+++ ssmc/src/main/java/com/prgrms/devcource/configures/WebSecurityConfigure.java
@@ -1,19 +1,12 @@
 package com.prgrms.devcource.configures;
 
+import com.prgrms.devcource.jwt.Jwt;
 import com.prgrms.devcource.user.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.expression.SecurityExpressionHandler;
-import org.springframework.security.access.vote.UnanimousBased;
-import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -22,20 +15,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
-import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -43,10 +27,16 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
     private UserService userService;
+    private JwtConfigure jwtConfigure;
 
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @Autowired
+    public void setJwtConfigure(JwtConfigure jwtConfigure) {
+        this.jwtConfigure = jwtConfigure;
     }
 
     @Override
@@ -64,54 +54,36 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public Jwt jwt() {
+        return new Jwt(
+                jwtConfigure.getIssuer(),
+                jwtConfigure.getClientSecret(),
+                jwtConfigure.getExpirySeconds()
+        );
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/me").hasAnyRole("USER", "ADMIN") //인증 영역
-                .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')") //isFullyAuthenticated 리멤머미로 인증되지 않은 사용자
+                .antMatchers("/api/user/me").hasAnyRole("USER", "ADMIN") //인증 영역
                 .anyRequest().permitAll() //익명영역
                 .and()
+                .csrf()//페이지 기반 아니기 때문에 필요없음
+                .disable()
+                .headers()
+                .disable()
                 .formLogin() //스프링 시큐리티가 로그인 폼 자동 생성
-                .defaultSuccessUrl("/") //로그인 성공시 갈 곳
-//                .loginPage("/my-login")//커스터마이징
-//                .usernameParameter("my-username")
-//                .passwordParameter("my-password")
-                .permitAll() //로그인은 권한 필요 없음
-                .and()
-                /**
-                 * 로그아웃 설정
-                 */
+                .disable()
+                .httpBasic()
+                .disable()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))//얘도 디폴트임
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)//생략해도 무관 이미 기본값에 설정
-                .clearAuthentication(true)
-                .and()
-//                .httpBasic()
-//                .and()
-                /**
-                 * remember me 설정
-                 */
+                .disable()
                 .rememberMe()
-                .rememberMeParameter("remember-me")
-                .tokenValiditySeconds(300)
-                .userDetailsService(getApplicationContext().getBean(UserDetailsService.class))
-                .and()
-                /**
-                 * http요청을 https요청으로 리다이렉트
-                 */
-                .requiresChannel()
-//                .antMatchers("/api/**")
-                .anyRequest().requiresSecure()  //모든 요청은 시큐어(https) 필요
-                .and()
+                .disable()
                 .sessionManagement()
-                .sessionFixation().changeSessionId()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                .invalidSessionUrl("/")
-                .maximumSessions(1)//최대 동시로그인 가능 개수
-                    .maxSessionsPreventsLogin(false)//기본 false
-                    .and()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 /**
                  * 예외처리핸들러
